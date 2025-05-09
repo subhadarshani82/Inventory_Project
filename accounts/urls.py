7a8da1f10f2748a1f456bdd862ef9c2eb9ed24fd
subhadarshani <subhadarshani82@gmail.com>
2025-05-09 22:26:27 +0530

    Create login logout functionalities.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import register_user,all_user
+from .views import register_user,all_user,login,logout
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('all_user/', all_user, name='all_user'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -29,16 +29,16 @@ def all_user(request):
     users=CustomUser.objects.all()
     return render(request,"accounts/all_user.html",{'users':users})
     
-# def login_view(request):
-#     if request.method == 'POST':
-#         username=request.POST['username']
-#         password=request.POST['password']
-#         user = authenticate(request,username=username,password=password)
-#         if user:
-#             login(request,user)
-#             return redirect('dashboard')
-#     return render (request,'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('dashboard')
+    return render (request,'accounts/login.html')
 
-# def logout_view(request):
-#     logout(request)
-#     return redirect('login')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
